dc0bb71351f3cf86a3ef8e1782263198186a44c5
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-12-06 22:34:24 -0500

    Create ThreadTest.java

---
+++ ThreadTest.java
@@ -0,0 +1,12 @@
+public class ThreadTest{
+    /**
+     * @author Michias Shiferaw
+     * @since November 16 2021
+     * @version 2.1
+     * @param args
+     */
+    public static void main(String[] args) {
+        FilePlayer trial = new FilePlayer();
+        trial.tune();
+    }
+}
