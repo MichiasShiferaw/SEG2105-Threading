a52eec2ce8989c6bf0fa0c41401813ef7ddf8866
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-12-06 22:26:03 -0500

    Create FilePlayer.java

---
+++ FilePlayer.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
